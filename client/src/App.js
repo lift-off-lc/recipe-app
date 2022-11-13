e92b6f2923ad88e0c612c68737fd3c4e6d7bc844
chloemyha <chloehuynh2401@gmail.com>
2022-11-13 00:19:10 -0500

    adding homepage features and routers path

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
+import Recipe from "./pages/Recipe";
+import RecipeList from "./pages/RecipeList";
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./index.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div className="page.body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recipe" element={<Recipe />} />
+            <Route path="/recipelist" element={<RecipeList />} />
+            // * prefer to render to other path that did not listed above
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
