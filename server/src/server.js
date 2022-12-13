19a9a4d7386401ae17fd34fadb29c2f9f2e33753
bdiddy2022 <brendan.t.downs@gmail.com>
2022-12-12 20:30:57 -0500

    Still lost

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { db, connectToDb } from "./db.js";
 import { ObjectId } from "mongodb";
 import cors from "cors";
@@ -10,7 +10,7 @@ const credentials = JSON.parse(
 );
 
 admin.initializeApp({
-  credential: admin.credential.cert(credentials)
+  credential: admin.credential.cert(credentials),
 })
 
 const app = express();
@@ -19,6 +19,14 @@ app.use(cors());
 
 //auto-loads user **info** whenever FE sends/requsets any related info
 app.use(express.json());
+
+
+//connect to database
+connectToDb(() => {
+  app.listen(8000, () => {
+    console.log("Server listening on 8000"); //this is our backend server
+  });
+});
 
 //loads user **credentials** when needed
 app.use(async (req, res, next) => {
@@ -32,18 +40,18 @@ app.use(async (req, res, next) => {
       req.user = user;
   } catch (e){
     //sends error if no such user and/or token, or if no auth token received
-    res.sendStatus(400);
+    return res.sendStatus(400);
   }
   }
+
+  req.user = req.user || {};
   next();
 });
 
 
 //display recipe details
 app.get("/recipe/:id", async (req, res) => {
-  //checks user id
-  //const {uid} = req.user;
- 
+  //checks recipe id
   const { id } = req.params;
   const recipe = await db.collection("recipes").findOne({ _id: ObjectId(id) });
   recipe ? res.json(recipe) : res.status(404);
@@ -57,7 +65,18 @@ app.get("/recipe", (req, res) => {
     .then((documents) => res.send(documents));
 });
 
-//TODO: add recipe
+//TODO: Add recipe to server
+
+//  "Gatekeeper" method.
+//Checks that user is logged in before allowing them to continue.
+//IMPORTANT: next() needs to be located ABOVE any of this file's methods that require a logged-in account, and BELOW anything that doesn't
+app.use((req, res, next) => {
+  if(req.user) { 
+    next();
+  } else {
+    return res.sendStatus(401);
+  }
+});
 
 //delete recipe
 app.delete("/recipe/:id", async (req, res) => {
@@ -80,15 +99,24 @@ app.get("/favorite", async (req, res) => {
 });
 
 // add recipeId to favorite list of recipes
-app.post("/favorite", (req, res) => {
+app.post("/favorite", async (req, res) => {
   const data = req.body;
-  db.collection("favorite").updateOne(
+  const { uid }= req.user;
+
+  const objs = await db.collection("favorite").find().toArray();
+  const ids = objs.map((obj) => ObjectId(obj.recipeId));
+  const previouslyFavorited = db.collection("recipes")
+    .find({ _id: { $in: ids } })
+  
+  if(!previouslyFavorited){
+    db.collection("favorite").updateOne(
     { recipeId: data._id },
     { $set: { isFavorite: true } },
 
     { upsert: true }
   );
-  res.json({ res: "Success" });
+  res.json({ res: "Success" });}
+  
 });
 
 //delete favorite recipe
@@ -125,11 +153,4 @@ app.delete("/shoppinglist", (req, res) => {
   const data = req.body;
   db.collection("shoppinglist").deleteOne({ recipeId: data._id });
   res.json({ res: "Deleted successfully" });
-});
-
-//connect to database
-connectToDb(() => {
-  app.listen(8000, () => {
-    console.log("Server listening on 8000"); //this is our backend server
-  });
 });
